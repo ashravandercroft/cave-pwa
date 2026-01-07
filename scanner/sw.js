4692b09c764fdd19a886843ef88f7fc0c5d770c0
ashravandercroft <sarah.chauveau.home@gmail.com>
2026-01-07 22:30:39 +0100

    Update sw.js

--- scanner/sw.js
+++ scanner/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "scanner-cache-v1";
+const CACHE = "scanner-cache-v2";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (evt) => {
