839f40f1f028fc800cf366c5a671fad78a0f8877
ashravandercroft <sarah.chauveau.home@gmail.com>
2026-01-07 22:37:09 +0100

    Update app.js

--- scanner/app.js
+++ scanner/app.js
@@ -1,4 +1,5 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbyxfNO9zWm3CT-GACd0oQE_ambHcJ33VHrQOxVxQIIEEpuv53G_A08cWqHXOsYcofaD/exec";
+alert("scanner v2 chargé");
 
 const $ = (id) => document.getElementById(id);
 
