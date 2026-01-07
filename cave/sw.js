4ec0251c99f18aa0e067b83a2281478f258584cd
ashravandercroft <sarah.chauveau.home@gmail.com>
2026-01-07 22:03:22 +0100

    Update sw.js

--- cave/sw.js
+++ cave/sw.js
@@ -1,1 +1,17 @@
+const CACHE = "cave-cache-v1";
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
 
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((cached) => cached || fetch(evt.request))
+  );
+});
